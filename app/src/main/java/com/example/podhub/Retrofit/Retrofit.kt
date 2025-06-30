bdc9c29d9fc25e0139742b663a7dbb82f46fe5ca
ToanTrinh2004 <2251120326@ut.edu.vn>
2025-06-30 17:49:49 +0700

    fix retrofit

--- app/src/main/java/com/example/podhub/Retrofit/Retrofit.kt
+++ app/src/main/java/com/example/podhub/Retrofit/Retrofit.kt
@@ -4,6 +4,8 @@ package com.example.podhub.Retrofit
 
 
 
+import com.example.podhub.Service.ArtistService
+import com.example.podhub.Service.PodcastService
 import com.example.podhub.Service.UserService
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
@@ -18,9 +20,9 @@ object RetrofitInstance {
     private const val SECOND_URL = "http://192.168.1.5:3000"
 
     private val okHttpClient = OkHttpClient.Builder()
-        .connectTimeout(10, TimeUnit.SECONDS)
-        .readTimeout(10, TimeUnit.SECONDS)
-        .writeTimeout(10, TimeUnit.SECONDS)
+        .connectTimeout(60, TimeUnit.SECONDS)
+        .readTimeout(60, TimeUnit.SECONDS)
+        .writeTimeout(60, TimeUnit.SECONDS)
         .build()
 
     private val retrofit = Retrofit.Builder()
@@ -32,5 +34,13 @@ object RetrofitInstance {
     val userService: UserService by lazy {
         retrofit.create(UserService::class.java)
     }
+    val artistService: ArtistService by lazy {
+        retrofit.create(ArtistService::class.java)
+    }
+    val podcastService: PodcastService by lazy {
+        retrofit.create(PodcastService::class.java)
+    }
+
+
 }
 
